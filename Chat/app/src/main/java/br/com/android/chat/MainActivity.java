0033634756c1d705d2f676b6da51211c7cf7dbeb
ttuca123 <ttuca123@gmail.com>
2017-03-30 23:16:38 -0300

    Aula 3

--- Chat/app/src/main/java/br/com/android/chat/MainActivity.java
+++ Chat/app/src/main/java/br/com/android/chat/MainActivity.java
@@ -1,13 +1,13 @@
 package br.com.android.chat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,12 +18,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton btnAdd = (FloatingActionButton) findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent it = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(it);
             }
         });
     }
